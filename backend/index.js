2ba41751c56aeae6ed93f4f82ff058e5941c1986
asamad35 <samad.abdus3535@gmail.com>
2023-02-23 21:26:03 +0530

    added mssg hidePic functionality, added mssg sound effects

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,8 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://localhost:5173",
+    // origin: "https://socio-plus.netlify.app",
     methods: ["GET", "POST"],
   },
 });
